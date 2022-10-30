d67453491439a233e072837f1727baa654c2f30e
elyad <ella.dikovich@gmail.com>
2022-10-30 20:40:36 +0100

    katucha3

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,14 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public static void main(String[] args) {
-        revert(489);
+        revert(527);
     }
 
     public static void revert(int number) {
         int a = number / 100;
-        int b = number / 10 - a * 10;
-        int c = number - a * 100 - b * 10;
+        int b = (number / 10) - (a * 10);
+        int c = number - (a * 100) - (b * 10);
 
-        System.out.println(a + "\n" + b + "\n" + c);
+        System.out.println(c + "" + b + "" + a);
     }
 }
